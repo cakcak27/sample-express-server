d38eb97d031d9c5ead7eda440d4984d79c2750e6
cakcak27 <cahyadiikbal@gmail.com>
2021-08-08 21:42:29 +0700

    menambah authentikasi user

--- app.js
+++ app.js
@@ -16,11 +16,12 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-require("./routes/tutorial.routes.js")(app)
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 const db = require("./models");
+const Role = db.role;
+
 db.mongoose
   .connect(db.url, {
     useNewUrlParser: true,
@@ -28,11 +29,54 @@ db.mongoose
   })
   .then(() => {
     console.log("Connected to the database!");
+    initial();
   })
   .catch(err => {
     console.log("Cannot connect to the database!", err);
     process.exit();
   });
+
+function initial(){
+  Role.estimatedDocumentCount((err, count)=>{
+    if(!err && count === 0) {
+      new Role({
+        name: "user"
+      }).save(err =>{
+        if(err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'user' to roles collection");
+      });
+
+      new Role({
+        name: "moderator"
+      }).save(err => {
+        if(err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'moderator' to roles collection");
+      });
+
+      new Role({
+        name: "admin"
+      }).save(err => {
+        if(err) {
+          console.log("error", err)
+        }
+
+        console.log("added 'admin' to roles collection")
+      })
+    }
+  })
+}
+
+//routes
+require("./routes/tutorial.routes")(app)
+require("./routes/auth.routes")(app);
+require("./routes/user.routes")(app);
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
--- models/index.js
+++ models/index.js
@@ -5,5 +5,11 @@ mongoose.Promise = global.Promise;
 const db = {}
 db.mongoose = mongoose;
 db.url = url;
+
+db.user = require("./user.model")(mongoose);
+db.role = require("./role.model")(mongoose);
+
+db.ROLES = ["user", "admin", "moderator"];
+
 db.tutorials = require('./tutorial.model.js')(mongoose);
 module.exports = db;
